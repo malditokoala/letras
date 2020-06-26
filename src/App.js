2a65e4cc621e5a89e98a7142e83ec4c10a8f764b
Edgar Gomez Borboa <edgargomezborboa@hotmail.com>
2020-06-26 10:12:39 -0700

    Proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Fragment, useState, useEffect } from "react";
-import Formulario from "./components/Formulario";
-import Cancion from "./components/Cancion";
-import axios from "axios";
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Info from './components/Info';
+import axios from 'axios';
 
 function App() {
   // Definir el state
   const [busquedaletra, guardarBusquedaLetra] = useState({});
-  const [letra, guardarLetra] = useState("");
+  const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
     const consultarAPI = async () => {
       const { artista, cancion } = busquedaletra;
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const resultado = await axios(url);
-      console.log(resultado.data.lyrics);
-      guardarLetra(resultado.data.lyrics);
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      const [letra, informacion] = await Promise.all([axios(url), axios(url2)]);
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
     };
     consultarAPI();
   }, [busquedaletra]);
@@ -22,12 +26,14 @@ function App() {
   return (
     <Fragment>
       <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-md-6">
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <Info info={info} />
+          </div>
+          <div className='col-md-6'>
             <Cancion letra={letra} />
           </div>
-          <div className="col-md-6"></div>
         </div>
       </div>
     </Fragment>
--- src/components/Cancion.js
+++ src/components/Cancion.js
@@ -1,10 +1,13 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
-const Cancion = ({ letra }) => (
-  <Fragment>
-    <h2>Letra Cancion</h2>
-    <p className="letra">{letra}</p>
-  </Fragment>
-);
+const Cancion = ({ letra }) => {
+  if (letra.length === 0) return null;
+  return (
+    <Fragment>
+      <h2>Letra Cancion</h2>
+      <p className='letra'>{letra}</p>
+    </Fragment>
+  );
+};
 
 export default Cancion;
